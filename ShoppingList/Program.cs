99a8ffde74adbb563fabc45e33274c8b4593e28a
Diana Madissoo <dmadisso@tktk.ee>
2022-01-04 21:08:44 +0200

    first commit

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\diana\Desktop\Programmeerimise alused\Samples";
+            string newDirectory = ($"ShoppingList");
+            string fileName = ($"myShoppingList.txt");
+
+
+            Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+            File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+
+            
+
+        }
+    }
+}
---
+++ wishList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace wishList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myWishList = new List<string>();
+
+            Console.WriteLine(myWishList.Count);
+
+            myWishList.Add("coffe machine");
+            myWishList.Add("new pair of socks");
+            myWishList.Add("box of chocolates");
+            myWishList.Add("new pillow");
+
+            Console.WriteLine(myWishList.Count);
+
+            Console.WriteLine(myWishList[0]);
+            Console.WriteLine(myWishList[1]);
+            Console.WriteLine(myWishList[2]);
+
+            foreach(string wish in myWishList)
+            {
+                Console.WriteLine(wish);
+            }
+
+
+        }
+    }
+}
